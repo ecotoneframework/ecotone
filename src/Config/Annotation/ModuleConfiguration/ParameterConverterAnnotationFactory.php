29ae826ee627652aa080faf5d0be999b5b8be3c6
Dariusz Gafka <dgafka.mail@gmail.com>
2018-05-10 16:57:35 +0200

    configure parameter converters without registering

--- src/Config/Annotation/ModuleConfiguration/ParameterConverterAnnotationFactory.php
+++ src/Config/Annotation/ModuleConfiguration/ParameterConverterAnnotationFactory.php
@@ -45,32 +45,45 @@ class ParameterConverterAnnotationFactory
      */
     public function configureParameterConverters(MessageHandlerBuilderWithParameterConverters $messageHandlerBuilder, string $relatedClassName, string $methodName, array $parameterConverterAnnotations): void
     {
-        $interfaceToCall = InterfaceToCall::create($relatedClassName, $methodName);
+        $messageHandlerBuilder->withMethodParameterConverters($this->createParameterConverters($messageHandlerBuilder, $relatedClassName, $methodName, $parameterConverterAnnotations));
+    }
+
+    /**
+     * @param MessageHandlerBuilderWithParameterConverters $messageHandlerBuilder
+     * @param string                                       $relatedClassName
+     * @param string                                       $methodName
+     * @param array                                        $parameterConverterAnnotations
+     *
+     * @return array
+     */
+    public function createParameterConverters(MessageHandlerBuilderWithParameterConverters $messageHandlerBuilder, string $relatedClassName, string $methodName, array $parameterConverterAnnotations): array
+    {
+        $interfaceToCall     = InterfaceToCall::create($relatedClassName, $methodName);
         $parameterConverters = [];
 
         foreach ($parameterConverterAnnotations as $parameterConverterAnnotation) {
             if ($parameterConverterAnnotation instanceof MessageToHeaderParameterAnnotation) {
                 $parameterConverters[] = MessageToHeaderParameterConverterBuilder::create($parameterConverterAnnotation->parameterName, $parameterConverterAnnotation->headerName)
-                                            ->setRequired($parameterConverterAnnotation->isRequired);
+                    ->setRequired($parameterConverterAnnotation->isRequired);
             } else if ($parameterConverterAnnotation instanceof MessageToPayloadParameterAnnotation) {
                 $parameterConverters[] = MessageToPayloadParameterConverterBuilder::create($parameterConverterAnnotation->parameterName);
             } else if ($parameterConverterAnnotation instanceof MessageParameterAnnotation) {
                 $parameterConverters[] = MessageParameterConverterBuilder::create($parameterConverterAnnotation->parameterName);
             } else if ($parameterConverterAnnotation instanceof MessageToReferenceServiceAnnotation) {
-                $parameter = $interfaceToCall->getParameterWithName($parameterConverterAnnotation->parameterName);
+                $parameter     = $interfaceToCall->getParameterWithName($parameterConverterAnnotation->parameterName);
                 $referenceName = $parameterConverterAnnotation->referenceName ? $parameterConverterAnnotation->referenceName : $parameter->getTypeHint();
                 $messageHandlerBuilder->registerRequiredReference($referenceName);
 
                 $parameterConverters[] = MessageToReferenceServiceParameterConverterBuilder::create(
                     $parameterConverterAnnotation->parameterName, $referenceName, $messageHandlerBuilder
                 );
-            }else if ($parameterConverterAnnotation instanceof MessageToExpressionParameterAnnotation) {
+            } else if ($parameterConverterAnnotation instanceof MessageToExpressionParameterAnnotation) {
                 $parameterConverters[] = MessageToExpressionEvaluationParameterConverterBuilder::createWith(
                     $parameterConverterAnnotation->parameterName, $parameterConverterAnnotation->expression, $messageHandlerBuilder
                 );
             }
         }
 
-        $messageHandlerBuilder->withMethodParameterConverters($parameterConverters);
+        return $parameterConverters;
     }
 }

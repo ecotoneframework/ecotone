8797cd3b6c76a8680a67dd44111c013f40a55e5f
dgafka <dgafka.mail@gmail.com>
2019-09-05 17:58:15 +0200

    echo logger

---
+++ src/Messaging/Handler/Logger/EchoLogger.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Ecotone\Messaging\Handler\Logger;
+
+use Psr\Log\AbstractLogger;
+
+/**
+ * Class EchoLogger
+ * @package Ecotone\Messaging\Handler\Logger
+ * @author Dariusz Gafka <dgafka.mail@gmail.com>
+ */
+class EchoLogger extends AbstractLogger
+{
+    /**
+     * @inheritDoc
+     */
+    public function log($level, $message, array $context = array())
+    {
+        echo "{$level}: {$message}\n";
+    }
+}

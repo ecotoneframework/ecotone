e5bea4c71de4723ff2cf5171dbf8973e577f09e3
dgafka <dgafka.mail@gmail.com>
2019-12-09 16:52:35 +0100

    remove docblocks from publisher

--- src/Messaging/Publisher.php
+++ src/Messaging/Publisher.php
@@ -12,29 +12,12 @@ use Ecotone\Messaging\Conversion\MediaType;
  */
 interface Publisher
 {
-    /**
-     * @param string $data
-     * @param string $sourceMediaType
-     */
+
     public function send(string $data, string $sourceMediaType = MediaType::TEXT_PLAIN) : void;
 
-    /**
-     * @param string $data
-     * @param string $sourceMediaType
-     * @param string[] $metadata
-     */
     public function sendWithMetadata(string $data, array $metadata, string $sourceMediaType = MediaType::TEXT_PLAIN) : void;
 
-    /**
-     * @param $data
-     * @return void
-     */
     public function convertAndSend(object $data) : void;
 
-    /**
-     * @param $data
-     * @param string[] $metadata
-     * @return void
-     */
     public function convertAndSendWithMetadata(object $data, array $metadata) : void;
 }

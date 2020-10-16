26e86905b47eeb1182dcc0e3281ab3cf0a638f93
Dariusz Gafka <dgafka.mail@gmail.com>
2020-10-16 16:15:57 +0200

    rector config

--- rector.php
+++ rector.php
@@ -9,7 +9,8 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 //    $services->set(Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector::class);
-//    $services->set(Rector\TypeDeclaration\Rector\Property\TypedPropertyRector::class);
+    $services->set(Rector\TypeDeclaration\Rector\Property\TypedPropertyRector::class);
+    $services->set(Rector\Php74\Rector\Property\TypedPropertyRector::class);
 
     // get parameters
     $parameters = $containerConfigurator->parameters();
@@ -18,6 +19,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::SETS, [
         SetList::DEAD_CODE,
     ]);
+    $parameters->set('php_version_features', '7.4');
 
     $parameters->set(Option::EXCLUDE_RECTORS, [
         \Rector\DeadCode\Rector\FunctionLike\RemoveOverriddenValuesRector::class,
@@ -26,7 +28,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector::class,
         Rector\DeadCode\Rector\Property\RemoveSetterOnlyPropertyAndMethodCallRector::class,
         Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector::class,
-        Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector::class
+        Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector::class,
+        Rector\DeadCode\Rector\ClassMethod\RemoveDeadConstructorRector::class
     ]);
 
     // get services (needed for register a single rule)

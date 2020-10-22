5f13b6e822aef786d7de7b4ef29d22e05414fbaa
Dariusz Gafka <dgafka.mail@gmail.com>
2020-10-22 17:38:31 +0200

    Remove expression annotation

--- tests/Messaging/Unit/Config/Annotation/ParameterConverterAnnotationFactoryTest.php
+++ tests/Messaging/Unit/Config/Annotation/ParameterConverterAnnotationFactoryTest.php
@@ -3,17 +3,14 @@ declare(strict_types=1);
 
 namespace Test\Ecotone\Messaging\Unit\Config\Annotation;
 
-use Ecotone\Messaging\Annotation\Parameter\Expression;
 use Ecotone\Messaging\Annotation\Parameter\Headers;
 use Ecotone\Messaging\Annotation\Parameter\Reference;
 use Ecotone\Messaging\Config\Annotation\ModuleConfiguration\ParameterConverterAnnotationFactory;
 use Ecotone\Messaging\Handler\InterfaceToCall;
 use Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter\AllHeadersBuilder;
-use Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter\HeaderExpressionBuilder;
 use Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter\MessageConverterBuilder;
 use Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter\PayloadBuilder;
 use Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter\ReferenceBuilder;
-use Ecotone\Messaging\Handler\Processor\MethodInvoker\ConverterBuilder;
 use Test\Ecotone\Messaging\Fixture\Annotation\MessageEndpoint\ServiceActivator\AllConfigurationDefined\ServiceActivatorWithAllConfigurationDefined;
 use Test\Ecotone\Messaging\Unit\MessagingTest;
 

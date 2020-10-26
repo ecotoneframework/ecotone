d8644bab7624612746ce8c25821cf1bd1ed1562c
Dariusz Gafka <dgafka.mail@gmail.com>
2020-10-26 16:51:45 +0100

    remove unused code

--- src/Modelling/Annotation/AggregateIdentifier.php
+++ src/Modelling/Annotation/AggregateIdentifier.php
@@ -7,18 +7,4 @@ use Ecotone\Messaging\Support\Assert;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 class AggregateIdentifier
 {
-    /**
-     * Name of the routing key property on messages that provides the identifier
-     */
-    public string $targetIdentifierName;
-
-    public function __construct(string $targetIdentifierName = "")
-    {
-        $this->targetIdentifierName = $targetIdentifierName;
-    }
-
-    public function getTargetIdentifierName(): string
-    {
-        return $this->targetIdentifierName;
-    }
 }

2a817c2615d2723f42815e721d97b4cbe8bfd44f
Dariusz Gafka <dgafka.mail@gmail.com>
2020-12-09 15:57:56 +0100

    introduce distributed attribute

--- src/Modelling/Annotation/Distributed.php
+++ src/Modelling/Annotation/Distributed.php
@@ -6,14 +6,14 @@ namespace Ecotone\Modelling\Annotation;
 #[\Attribute]
 class Distributed
 {
-    private string $distributionReference;
+    private ?string $distributionReference;
 
-    public function __construct(string $distributionReference = "default")
+    public function __construct(string $distributionReference = null)
     {
         $this->distributionReference = $distributionReference;
     }
 
-    public function getDistributionReference(): string
+    public function getDistributionReference(): ?string
     {
         return $this->distributionReference;
     }

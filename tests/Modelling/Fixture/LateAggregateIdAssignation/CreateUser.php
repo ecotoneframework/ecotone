c04b88f79b774bbb697593519263633eecd2a5fa
Dariusz Gafka <dariuszgafka@gmail.com>
2022-04-09 11:12:18 +0200

    Adjust test to run against php 8.0

--- tests/Modelling/Fixture/LateAggregateIdAssignation/CreateUser.php
+++ tests/Modelling/Fixture/LateAggregateIdAssignation/CreateUser.php
@@ -4,7 +4,7 @@ namespace Test\Ecotone\Modelling\Fixture\LateAggregateIdAssignation;
 
 class CreateUser
 {
-    public function __construct(public readonly string $name)
+    public function __construct(public string $name)
     {
 
     }

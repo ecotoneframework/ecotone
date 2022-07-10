7306b9a8ce4a012f24a58787d794821442ec6f1d
Dariusz Gafka <dariuszgafka@gmail.com>
2022-07-10 13:38:55 +0200

    Set up monorepo for Core Module

---
+++ monorepo-builder.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::DATA_TO_APPEND, [
+        ComposerJsonSection::AUTOLOAD_DEV => [
+            'psr-4' => [
+                "Tests\\Ecotone\\" => "tests",
+                "IncorrectAttribute\\" => [
+                    "tests\\AnnotationFinder\\Fixture\\Usage\\Attribute\\TestingNamespace\\IncorrectAttribute\\TestingNamespace"
+                ],
+            ],
+        ],
+
+        ComposerJsonSection::REQUIRE_DEV => [
+            "behat/behat" => "^3.10",
+            "php-coveralls/php-coveralls" => "^2.5",
+            "phpstan/phpstan" => "^1.7",
+            "phpunit/phpunit" => "^9.5",
+            "symfony/expression-language" => "^6.0",
+            "symplify/monorepo-builder" => "^11.0"
+        ],
+    ]);
+};
